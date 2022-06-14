ca4f8879ba72218d0ce036f39559a629c1e95137
Soka Adzahara Arieka <soka.adzahara@gmail.com>
2022-06-15 04:08:10 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+
+  const toggleComplete: ToggleComplete = selectedTodo => {
+    const updatedTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return { ...todo, complete: !todo.complete };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const addTodo: AddTodo = newTodo => {
+    if (newTodo !== "") {
+      setTodos([...todos, { text: newTodo, complete: false }]);
+    }
+  };
+
+  const removeTodo: RemoveTodo = todoToRemove => {
+    let updatedTodos: Array<Todo> = todos.filter(todo => todo.text != todoToRemove.text);
+    setTodos(updatedTodos);
+  }
+
+  const editTodo: EditTodo = todoToEdit => {
+    let todoToUpdateIndex: number = todos.findIndex(todo => todo.text == todoToEdit.text);
+    console.log(todoToUpdateIndex);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="todo-app">
+      <header>
+        <h1>
+        Todo App
+        </h1>
       </header>
+      <TodoForm addTodo={addTodo}/>
+      <TodoList todos={todos} toggleComplete={toggleComplete} onRemoveTodo={removeTodo} editTodo={editTodo}/>
     </div>
   );
-}
+};
 
 export default App;
